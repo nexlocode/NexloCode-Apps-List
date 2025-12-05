9a0f95b7ba3054d349382d9e10772b225d19db10
nexlocode <nexlocode@gmail.com>
2025-12-05 11:53:02 +0600

    Update magictext.js

--- magictext.js
+++ magictext.js
@@ -1,13 +1,7 @@
 // NexloCode Apps List
 // Generated on: 05/12/2025, 11:25:11
 
-const apps = [
-    {
-        "appName": "Magic Text",
-        "downloadUrl": "https://faruk0ahmed.github.io/NexloCode-Store/?id=1764874062659",
-        "version": "v1.0.0",
-        "updatedAt": "05/12/2025"
-    }
-];
-
-export default apps;
+{
+    "version": "1.1",
+    "downloadUrl": "https://play.google.com/store/apps/details?id=com.yourapp"
+}
