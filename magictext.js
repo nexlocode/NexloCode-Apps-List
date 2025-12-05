9272b6e0fc746e2df1f5da45dc8771046ec8be59
nexlocode <nexlocode@gmail.com>
2025-12-05 11:30:59 +0600

    Add files via upload

---
+++ magictext.js
@@ -0,0 +1,13 @@
+// NexloCode Apps List
+// Generated on: 05/12/2025, 11:25:11
+
+const apps = [
+    {
+        "appName": "Magic Text",
+        "downloadUrl": "https://faruk0ahmed.github.io/NexloCode-Store/?id=1764874062659",
+        "version": "v1.0.0",
+        "updatedAt": "05/12/2025"
+    }
+];
+
+export default apps;
